69fe96c5b41edf7e8e90f06ea57002f424d895c7
Hiago Lucas <hcardealdemelo@gmail.com>
2026-08-09 18:31:09 -0300

    implement frequencyTableToHeap

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,40 @@
 module Main where
 
+import qualified Data.ByteString as BS
+import Data.Word (Word8)
+import qualified Data.Map.Strict as Map
 import qualified Data.Heap as Heap
+
+data HuffmanTree 
+  = Leaf Word8 Int
+  | HuffmanNode Int HuffmanTree HuffmanTree
+  deriving (Show, Eq)
+
+weight :: HuffmanTree -> Int
+weight (Leaf _ w)          = w
+weight (HuffmanNode w _ _) = w
+
+instance Ord HuffmanTree where
+  compare t1 t2 = compare (weight t1) (weight t2)
+
+fileAsByteList :: FilePath -> IO [Word8]
+fileAsByteList path = do
+  content <- BS.readFile path
+  return (BS.unpack content)
+
+
+getFrequencyTable :: [Word8] -> Map.Map Word8 Integer
+getFrequencyTable = foldl' (\acc w -> Map.insertWith (+) w 1 acc) Map.empty
+
+
+frequencyTableToHeap :: Map.Map Word8 Int -> Heap.MinHeap HuffmanTree
+frequencyTableToHeap freqMap = Heap.fromList [Leaf byte count | (byte, count) <- Map.toList freqMap]
+
 
 main :: IO ()
 main = do
-  -- Criando um MinHeap com números
-  let heapExemplo = Heap.fromList [10, 3, 5, 1] :: Heap.MinHeap Int
+  -- Exemplo: lendo um arquivo qualquer
+  bytes <- fileAsByteList "arquivo.txt"
   
-  putStrLn "Menor elemento do Heap:"
-  print (Heap.viewHead heapExemplo)
+  putStrLn "Primeiros 10 bytes em decimal (0-255):"
+  print (take 10 bytes)
